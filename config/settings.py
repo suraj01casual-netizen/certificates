8b406c9bd3d0047cbb8ab772894c409733b39ecf
Suraj-km09 <surajkumar.sm09@gmail.com>
2026-09-03 20:15:00 +0530

    Safeguard dj_database_url import with fallback in settings.py

--- config/settings.py
+++ config/settings.py
@@ -108,24 +108,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-import dj_database_url
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 _db_env = os.environ.get("DATABASE_URL")
 if _db_env:
-    DATABASES = {
-        "default": dj_database_url.config(
+    try:
+        import dj_database_url
+        DATABASES["default"] = dj_database_url.config(
             default=_db_env,
             conn_max_age=600,
             conn_health_checks=True,
         )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+    except ImportError:
+        pass
 
 
 # Password validation
